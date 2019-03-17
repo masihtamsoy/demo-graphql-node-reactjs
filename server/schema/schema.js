664f5ffc756a4fc9a129f3bfe3d3605058585f5e
masihtamsoy <masihtamsoy@gmail.com>
2019-03-17 16:52:11 +0530

    Use GraphQLID for id on Book Object Type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,11 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+} = graphql;
 
 const books = [
   {id: 1, name: 'Kite Runner', genre: 'politics'},
@@ -11,7 +16,7 @@ const books = [
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: {type: GraphQLString},
+    id: {type: GraphQLID},
     name: {type: GraphQLString},
     genre: {type: GraphQLString},
   })
@@ -22,7 +27,7 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: {id: {type: GraphQLString}},
+      args: {id: {type: GraphQLID}},
       resolve(parent, args) {
         return books.find((book) => book.id == args.id);
       }
